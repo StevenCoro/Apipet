cf387e9bb90f612af34c7e4fdd3812f3a1697792
StevenCoro <steven-coro@hotmail.com>
2018-08-10 18:56:42 -0500

    Me faltó esto xd

---
+++ app/src/main/java/apipet/web/apipet/SplashScreen.java
@@ -0,0 +1,46 @@
+package apipet.web.apipet;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+    private static int pantallaEspera=2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+        hideNavigationBar();
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                Intent siguientePantalla = new Intent(SplashScreen.this, InicioSesion.class);
+                startActivity(siguientePantalla);
+                finish();
+            }
+        }, pantallaEspera);
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        hideNavigationBar();
+    }
+
+    public void hideNavigationBar(){
+        this.getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_FULLSCREEN|
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION|
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY|
+                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|
+                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION|
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+    }
+}
