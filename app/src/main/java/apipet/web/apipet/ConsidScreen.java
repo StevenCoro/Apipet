8ebe96ee57e1b6b6a9103172a799d2c8391e02c2
StevenCoro <steven-coro@hotmail.com>
2018-08-11 15:11:00 -0500

    Arreglos

--- app/src/main/java/apipet/web/apipet/ConsidScreen.java
+++ app/src/main/java/apipet/web/apipet/ConsidScreen.java
@@ -4,26 +4,15 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 
-public class MainScreen extends AppCompatActivity {
+public class ConsidScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_main_screen);
+        setContentView(R.layout.activity_consid_screen);
         hideNavigationBar();
     }
-
-    @Override
-    protected void onPostResume() {
-        super.onPostResume();
-        hideNavigationBar();
-    }
-
     public void hideNavigationBar(){
         this.getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_FULLSCREEN|
@@ -33,13 +22,14 @@ public class MainScreen extends AppCompatActivity {
                         View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION|
                         View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
     }
-    public void Mascotas(View view){
-        Intent i = new Intent(this, MascotasScreen.class);
-        startActivity(i);
-        finish();
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        hideNavigationBar();
     }
-    public void Consideraciones(View view){
-        Intent i = new Intent(this, ConsidScreen.class);
+    public void Back(View view){
+        Intent i = new Intent(this, MascotasScreen.class);
         startActivity(i);
         finish();
     }
