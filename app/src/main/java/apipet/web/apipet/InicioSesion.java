87b0ca36c26d14d66e7958563ddde3ea83ca35d1
StevenCoro <steven-coro@hotmail.com>
2018-08-11 14:34:19 -0500

    Arreglos

--- app/src/main/java/apipet/web/apipet/InicioSesion.java
+++ app/src/main/java/apipet/web/apipet/InicioSesion.java
@@ -1,5 +1,6 @@
 package apipet.web.apipet;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -26,5 +27,10 @@ public class InicioSesion extends AppCompatActivity {
                         View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|
                         View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION|
                         View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+    }
+    public void Ingresar(View view){
+        Intent i = new Intent(this, MainScreen.class);
+        startActivity(i);
+        finish();
     }
 }
